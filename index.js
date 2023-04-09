64f7be4a67538b907ea7e775b169c90996a4c732
blaze <Arnnav0kudale@gmail.com>
2023-04-08 22:39:57 -0400

    added a confirm msg for when it starts made it cleaner

--- index.js
+++ index.js
@@ -2,37 +2,39 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const request = require('node-fetch');
-app.listen(8080)
 
 let whitelist = ['http://localhost:8080']
 
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(whitelist.indexOf(origin) === -1){
-      var message = "The CORS policy for this origin doesn't " +
+    origin: function(origin, callback) {
+        if (!origin) return callback(null, true);
+        if (whitelist.indexOf(origin) === -1) {
+            var message = "The CORS policy for this origin doesn't " +
                 'allow access from the particular origin.';
-      return callback(new Error(message), false);
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
     }
-    return callback(null, true);
-  }
 }));
 
 app.get('/info', (req, res) => {
-    return res.json({
-    })
-})
+    return res.json({});
+});
 
 app.get('/', (req, res) => {
     return res.json({
         "message": "Nothing to see here!"
-    })
-})
+    });
+});
 
 app.use((req, res, next) => {
     let json = {
         "error": "invalid_endpoint",
         "code": 404
     }
-    res.status(404).json(json)
-})
+    res.status(404).json(json);
+});
+
+app.listen(8080, () => {
+    console.log(`App listening on port 8080`)
+});
