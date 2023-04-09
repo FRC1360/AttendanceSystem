b050a5a104e1d98e7ddbaf4f1747a60cbc7504e2
blaze <Arnnav0kudale@gmail.com>
2023-04-08 22:20:52 -0400

    base code for API

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const request = require('node-superfetch');
+app.listen(8080)
+
+let whitelist = ['http://localhost:8080']
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(whitelist.indexOf(origin) === -1){
+      var message = "The CORS policy for this origin doesn't " +
+                'allow access from the particular origin.';
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+app.get('/info', (req, res) => {
+    return res.json({
+    })
+})
+
+app.get('/', (req, res) => {
+    return res.json({
+        "message": "Nothing to see here!"
+    })
+})
+
+app.use((req, res, next) => {
+    let json = {
+        "error": "invalid_endpoint",
+        "code": 404
+    }
+    res.status(404).json(json)
+})
