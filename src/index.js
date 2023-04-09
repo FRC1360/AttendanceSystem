ac6f20ed288dffc3dcb826a6263b9f87cb901d3c
blaze <Arnnav0kudale@gmail.com>
2023-04-08 22:55:27 -0400

    moved everything to src

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const database = require("mongoose");
 const chalk = require("chalk");
+const databaseConnection = require("./utils/Database.js");
+let whitelist = ['http://localhost:8080'];
 
-database.connect("mongodb+srv://admin:389179@cluster0.vccaflc.mongodb.net/?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-database.connection.on("connected", () => {
-    console.log(`[${chalk.greenBright("BOOT")}] Connected to MongoDB!\n`);
-});
-
-database.connection.on("err", err => {
-    console.log(`[${chalk.redBright("ERROR")}] Unable to connect to the MongoDB. Error:\n${err}\n`);
-});
-
-database.connection.on("disconnected", () => {
-    console.log(`[${chalk.blueBright("INFO")}] MongoDB connection is disconnected\n`);
-});
-
-let whitelist = ['http://localhost:8080']
 
 app.use(cors({
     origin: function(origin, callback) {
@@ -54,5 +37,6 @@ app.use((req, res, next) => {
 });
 
 app.listen(8080, () => {
-    console.log(`[${chalk.greenBright("BOOT")}] App listening on port 8080`)
+    console.log(`[${chalk.greenBright("BOOT")}] App listening on port 8080`);
+    databaseConnection.execute();
 });
---
+++ src/utils/Database.js
@@ -0,0 +1,23 @@
+const database = require("mongoose");
+const chalk = require("chalk");
+
+module.exports = {
+    execute: async function() {
+        database.connect("mongodb+srv://admin:389179@cluster0.vccaflc.mongodb.net/?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        
+        database.connection.on("connected", () => {
+            console.log(`[${chalk.greenBright("BOOT")}] Connected to MongoDB!\n`);
+        });
+        
+        database.connection.on("err", err => {
+            console.log(`[${chalk.redBright("ERROR")}] Unable to connect to the MongoDB. Error:\n${err}\n`);
+        });
+        
+        database.connection.on("disconnected", () => {
+            console.log(`[${chalk.blueBright("INFO")}] MongoDB connection is disconnected\n`);
+        });        
+    }
+};
